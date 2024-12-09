9f758eb5bda8a99ab8915fea31e9cb51db3f0d1d
James Herd <137074420+herd-j@users.noreply.github.com>
2024-12-09 21:52:47 +0000

    create some helper functions

--- market_analysis/dataset.py
+++ market_analysis/dataset.py
@@ -3,6 +3,9 @@ from pathlib import Path
 import typer
 from loguru import logger
 from tqdm import tqdm
+import datetime as dt
+import yfinance as yf
+import pandas as pd
 
 from market_analysis.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
 
@@ -10,19 +13,25 @@ app = typer.Typer()
 
 
 @app.command()
-def main(
-    # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    # ----------------------------------------------
+def get_stock_data(
+    ticker: str,
+    end_date: tuple,
+    days: int | float,
 ):
-    # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
-    # -----------------------------------------
+    
+    end_date = dt.datetime(*end_date)
+    start_date = end_date - dt.timedelta(days=days)
+
+    logger.info(f"Loading {ticker} data from {start_date} to {end_date}.")
+    df = yf.download(ticker, start=start_date, end=end_date).stack(future_stack=True)
+    logger.success(f"Success loading {ticker} data.")
+
+    df.index.names = ["date", "ticker"]
+    df.columns = pd.Index([col.lower().replace(" ", "_") for col in df.columns], name="price")
+    df.sort_index(ascending=True, inplace=True)
+    df.index = df.index.droplevel(level=1)
+
+    return df
 
 
 if __name__ == "__main__":
